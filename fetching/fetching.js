c0280791d10f3335d480cc3c584f1d90f0ff7ae0
Alex Li <lialex5883@gmail.com>
2022-06-04 15:27:21 -0400

    added api fetch for mmr data by puuid and fetch user data

--- fetching/fetching.js
+++ fetching/fetching.js
@@ -5,8 +5,24 @@ const getRankedData = async (name, id) => {
     const data = await resp.json();
     return data.data;
 };
+
+const getUserData = async (name, id) => {
+  const resp = await fetch(
+    `https://api.henrikdev.xyz/valorant/v1/account/${name}/${id}`
+  );
+  const data = await resp.json();
+  return data.data;
+};
+
+const getRankedDataByPUUID = async (puuid) => {
+  const resp = await fetch(
+    `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/na/${puuid}`
+  );
+  const data = await resp.json();
+  return data.data;
+};
   
-module.exports = { getRankedData }
+module.exports = { getRankedData, getUserData, getRankedDataByPUUID }
 
   // getRankedData("nugnug", "6135").then((data) => {
   //   console.log(data);
