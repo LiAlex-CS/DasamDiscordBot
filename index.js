decbd13c55461571a56c6ae90e3ac7e9438c78ae
Alex Li <lialex5883@gmail.com>
2022-06-05 14:18:21 -0400

    added get and set credentials

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { JSONHasValue, stringArrToString, mmrDataToString, updateNameAndTag, mmrDataSingleToString } = require('./services');
+const { JSONHasValue, stringArrToString, mmrDataToString, updateNameAndTag, mmrDataSingleToString, searchDbByNameAndTag, getModifiedArguments } = require('./services');
 const { getRankedData, getUserData, getRankedDataByPUUIDs } = require("./fetching/fetching");
-const { PREFIX, COMMANDS, ALL_COMMANDS, UNKNOWN_COMMAND, COMMAND_ERRORS, COMMAND_DESCRIPTIONS, RANKS_INTRO } = require("./constants/commands");
+const { PREFIX, COMMANDS, ALL_COMMANDS, UNKNOWN_COMMAND, COMMAND_ERRORS, COMMAND_DESCRIPTIONS, RANKS_INTRO, HAS_SPACES_REMINDER, SET_SMURF_SUCCESS } = require("./constants/commands");
 const Discord = require("discord.js");
 const config = require("./config.json");
 const db = require("./data/smurfCreds.json");
@@ -14,6 +14,7 @@ client.on("messageCreate", (message) => {
     const commandBody = message.content.slice(PREFIX.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
+    const argsAsString =  message.content.slice(PREFIX.length + command.length + 1);
 
 
     if (command === COMMANDS.getCommands) {
@@ -59,13 +60,14 @@ client.on("messageCreate", (message) => {
         }
     }
     else if (command === COMMANDS.getRankPlayer) {
-        if (args.length === 2) {
-            getRankedData(args[0], args[1])
+        if (args.length >= 2) {
+            getRankedData(stringArrToString(args.slice(0, -1)), args[args.length - 1])
                 .then((data) => { 
-                    if(data.status === 404){
+                    if(data.status !== 200){
                         throw data;
+                    }else{
+                        message.reply(mmrDataSingleToString(data));
                     }
-                    message.reply(mmrDataSingleToString(data));
                 }).catch((err)=>{
                     if(err.status === 404){
                         message.reply(stringArrToString(args) + '" ' + COMMAND_ERRORS.getRankPlayer);
@@ -80,28 +82,66 @@ client.on("messageCreate", (message) => {
         }
     }
     else if (command === COMMANDS.getSmurfCred) {
+        const modifiedArgs = getModifiedArguments(argsAsString);
+        if (modifiedArgs.length >= 2) {
+            const name = stringArrToString(modifiedArgs.slice(0, -1));
+            const tag = modifiedArgs[modifiedArgs.length - 1];
 
+            const data = searchDbByNameAndTag(name, tag, db.user_credentials);
+            if(Object.keys(data).length){
+                message.reply(`For account: ${name} #${tag}, Username: ${data.username} Password: ${data.password}`);
+            }
+            else{
+                message.reply(`User: ${name} #${tag} ` + COMMAND_ERRORS.getSmurfCred);
+            }
+        }
+        else {
+            message.reply(`"${command} ` + stringArrToString(modifiedArgs) + '" ' + COMMAND_ERRORS.getSmurfCred_invalidArgs)
+        }
     }
     else if (command === COMMANDS.setSmurf) {
-
+        const modifiedArgs = getModifiedArguments(argsAsString);
+        if (modifiedArgs.length === 4) {
+            let userData = db;
+            getUserData(modifiedArgs[0], modifiedArgs[1]).then((data)=>{
+                if(data.status !== 200){
+                    throw data
+                }
+                else {
+                    userData.user_credentials.push(
+                        {
+                            name: modifiedArgs[0],
+                            tag: modifiedArgs[1],
+                            puuid: data.data.puuid,
+                            username: modifiedArgs[2],
+                            password: modifiedArgs[3]
+                        }
+                    )
+                    fs.writeFileSync('./data/smurfCreds.json', JSON.stringify(userData));
+                    message.reply(SET_SMURF_SUCCESS);
+                }
+            }).catch((err)=>{
+                if(err.status === 404){
+                    message.reply(`${modifiedArgs[0]} and ${modifiedArgs[1]} ` + COMMAND_ERRORS.setSmurf);
+                }
+                else{
+                    message.reply('error: ' + err);
+                }
+            });
+            
+        }
+        else {
+            message.reply(`"${command} ` + stringArrToString(args) + '" ' + COMMAND_ERRORS.getSmurfCred_invalidArgs);
+            message.reply(HAS_SPACES_REMINDER);
+        }
     }
     else {
         message.reply(`"${command}" ` + UNKNOWN_COMMAND);
     }
 
     if (command === "test") {
-        message.reply(`commandBody: ${commandBody} args: ${args} command: ${command}`);
+        message.reply(`commandBody: ${commandBody} args: ${args} command: ${command} argsAsString: ${argsAsString}`);
     }
-
-    // if (command === "ping") {
-    //     const timeTaken = Date.now() - message.createdTimestamp;
-    //     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-    // }
-    // else if (command === "sum") {
-    //     const numArgs = args.map(x => parseFloat(x));
-    //     const sum = numArgs.reduce((counter, x) => counter += x);
-    //     message.reply(`The sum of all the arguments you provided is ${sum}!`);
-    // }
 });
 
 
--- services.js
+++ services.js
@@ -48,4 +48,68 @@ const updateNameAndTag = (dataArr) => {
 
 }
 
-module.exports = { JSONHasValue, stringArrToString, mmrDataToString, updateNameAndTag, mmrDataSingleToString }
+const searchDbByNameAndTag = (name, tag, data) => {
+    for(let i = 0; i < data.length; i++){
+        if(data[i].name === name && data[i].tag === tag){
+            return data[i];
+        }
+    }
+    return {};
+}
+
+const getModifiedArguments = (commandBody) => {
+    let inQuotation = false;
+    let args = [];
+    let startIndex = 0;
+    let endIndex = 0;
+    let prevStartIndex = null;
+    let prevEndIndex = null;
+    let numQuotations = 0;
+  
+    for (let i = 0; i < commandBody.length; i++) {
+      if (commandBody[i] === '"') {
+        inQuotation = !inQuotation;
+        numQuotations += 1;
+      }
+      if (inQuotation) {
+        if (i > 0 && commandBody[i - 1] === '"') {
+          startIndex = i;
+        }
+        if (i < commandBody.length - 1 && commandBody[i + 1] === '"') {
+          endIndex = i;
+        }
+      } else {
+        if (
+          (i > 0 && commandBody[i - 1] === " ") ||
+          (i === 0 && commandBody[i] !== " ")
+        ) {
+          startIndex = i;
+        }
+        if (
+          (i < commandBody.length - 1 && commandBody[i + 1] === " ") ||
+          (i === commandBody.length - 1 && commandBody[i] !== " ")
+        ) {
+          endIndex = i;
+        }
+      }
+      if (
+        (startIndex || endIndex) &&
+        commandBody[endIndex] !== '"' &&
+        endIndex >= startIndex &&
+        startIndex !== prevStartIndex &&
+        endIndex !== prevEndIndex
+      ) {
+        args.push(commandBody.slice(startIndex, endIndex + 1));
+        prevStartIndex = startIndex;
+        prevEndIndex = endIndex;
+      }
+    }
+    if(numQuotations%2 !== 0){
+      return [];
+    }
+    return args;
+  };
+
+
+
+module.exports = { JSONHasValue, stringArrToString, mmrDataToString, updateNameAndTag, mmrDataSingleToString, searchDbByNameAndTag, getModifiedArguments }
