d1ebd29a1aafaf92cc4c316ad7bea0cc647b30b7
Alex Li <lialex5883@gmail.com>
2022-06-07 20:25:37 -0400

    removed comments with mongo_db creds

--- constants/mongodb_consts.js
+++ constants/mongodb_consts.js
@@ -1,7 +1,6 @@
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
-// const MONGO_USERNAME = "DiscordBotUser";
+
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-// const MONGO_PASSWORD = "SBaTjjZtmJF2tC72";
 
 const uri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@accountdetails.hh9drpy.mongodb.net/?retryWrites=true&w=majority`;
 
