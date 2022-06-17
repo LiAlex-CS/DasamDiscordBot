ba5067745e1add8881b4c2ddcf3cadde1ce14365
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-06-17 11:47:34 -0400

    Add error handling in credentials_commands.js

--- command_functions/credentials_command.js
+++ command_functions/credentials_command.js
@@ -1,4 +1,8 @@
 const { COMMAND_ERRORS } = require("../constants/commands");
+const {
+  STATUS_CODES,
+  STATUS_CODE_MESSAGES,
+} = require("../constants/status_codes");
 
 const { getAccountByNameAndTag } = require("../data/mongoDb");
 
@@ -10,22 +14,31 @@ const credentials_command = (message, command, argsAsString) => {
     const name = stringArrToString(modifiedArgs.slice(0, -1));
     const tag = modifiedArgs[modifiedArgs.length - 1];
 
-    getAccountByNameAndTag(name, tag).then((data) => {
-      if (Object.keys(data).length) {
-        if (data.private) {
-          message.reply(
-            `User: ${name} #${tag} ` +
-              COMMAND_ERRORS.getSmurfCred_privateAccount
-          );
-        } else {
-          message.reply(
-            `For account: ${name} #${tag}, Username: ${data.username} Password: ${data.password}`
-          );
+    getAccountByNameAndTag(name, tag)
+      .then((data) => {
+        if (!data) {
+          throw data;
         }
-      } else {
-        message.reply(`User: ${name} #${tag} ` + COMMAND_ERRORS.getSmurfCred);
-      }
-    });
+        if (Object.keys(data).length) {
+          if (data.private) {
+            message.reply(
+              `User: ${name} #${tag} ` +
+                COMMAND_ERRORS.getSmurfCred_privateAccount
+            );
+          } else {
+            message.reply(
+              `For account: ${name} #${tag}, Username: ${data.username} Password: ${data.password}`
+            );
+          }
+        } else {
+          message.reply(`User: ${name} #${tag} ` + COMMAND_ERRORS.getSmurfCred);
+        }
+      })
+      .catch((err) => {
+        if (err.status === STATUS_CODES.notFound) {
+          message.reply(STATUS_CODE_MESSAGES.MongoDBApiDown);
+        }
+      });
   } else {
     message.reply(
       `"${command} ` +
