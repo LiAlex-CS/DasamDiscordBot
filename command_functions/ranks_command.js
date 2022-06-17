08ce5b884731b235b055b671471fe7c2632474f0
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-06-17 11:57:58 -0400

    Add error handling to ranks_command.js

--- command_functions/ranks_command.js
+++ command_functions/ranks_command.js
@@ -19,66 +19,71 @@ const {
 const ranks_command = (message, command, args) => {
   if (args.length <= 2) {
     let reply = RANKS_INTRO;
-    getAccounts().then((accountData, err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        getRankedDataByPUUIDs(accountData.map((user) => user.puuid))
-          .then((rankedData) => {
-            if (args.length == 1 && !JSONHasKey(args[0], RANK_EMOJIS)) {
-              message.reply(
-                args[0] + " " + COMMAND_ERRORS.getAllRanks_invalidRank
-              );
-            } else if (
-              args.length == 2 &&
-              (!JSONHasKey(args[0], RANK_EMOJIS) || !checkValidTierNum(args[1]))
-            ) {
-              message.reply(
-                args[0] +
-                  " " +
-                  args[1] +
-                  " " +
-                  COMMAND_ERRORS.getAllRanks_invalidRankOrTier
-              );
-            } else {
-              if (args[0] === "Radiant" && args.length !== 1) {
-                message.reply(COMMAND_ERRORS.getAllRanks_invalidTierRadiant);
+    getAccounts()
+      .then((accountData, err) => {
+        if (err) {
+          throw err;
+        } else {
+          getRankedDataByPUUIDs(accountData.map((user) => user.puuid))
+            .then((rankedData) => {
+              if (args.length == 1 && !JSONHasKey(args[0], RANK_EMOJIS)) {
+                message.reply(
+                  args[0] + " " + COMMAND_ERRORS.getAllRanks_invalidRank
+                );
+              } else if (
+                args.length == 2 &&
+                (!JSONHasKey(args[0], RANK_EMOJIS) ||
+                  !checkValidTierNum(args[1]))
+              ) {
+                message.reply(
+                  args[0] +
+                    " " +
+                    args[1] +
+                    " " +
+                    COMMAND_ERRORS.getAllRanks_invalidRankOrTier
+                );
               } else {
-                const combinedData = accountData.map((data, index) => ({
-                  rankedData: rankedData[index],
-                  accountData: data,
-                }));
-                const sortedCombinedData = combinedData.sort(
-                  (a, b) => b.rankedData.data.elo - a.rankedData.data.elo
-                );
-                const sortedAccountData = sortedCombinedData.map(
-                  (data) => data.accountData
-                );
-                const sortedRankedData = sortedCombinedData.map(
-                  (data) => data.rankedData
-                );
-                reply =
-                  reply +
-                  rankSpecificity(args, sortedRankedData, sortedAccountData);
+                if (args[0] === "Radiant" && args.length !== 1) {
+                  message.reply(COMMAND_ERRORS.getAllRanks_invalidTierRadiant);
+                } else {
+                  const combinedData = accountData.map((data, index) => ({
+                    rankedData: rankedData[index],
+                    accountData: data,
+                  }));
+                  const sortedCombinedData = combinedData.sort(
+                    (a, b) => b.rankedData.data.elo - a.rankedData.data.elo
+                  );
+                  const sortedAccountData = sortedCombinedData.map(
+                    (data) => data.accountData
+                  );
+                  const sortedRankedData = sortedCombinedData.map(
+                    (data) => data.rankedData
+                  );
+                  reply =
+                    reply +
+                    rankSpecificity(args, sortedRankedData, sortedAccountData);
 
-                if (
-                  rankSpecificity(args, sortedRankedData, sortedAccountData)
-                    .length === 0
-                ) {
-                  reply = COMMAND_ERRORS.getAllRanks_noAccounts;
+                  if (
+                    rankSpecificity(args, sortedRankedData, sortedAccountData)
+                      .length === 0
+                  ) {
+                    reply = COMMAND_ERRORS.getAllRanks_noAccounts;
+                  }
+
+                  message.reply(reply);
+
+                  updateNameAndTag(rankedData);
                 }
-
-                message.reply(reply);
-
-                updateNameAndTag(rankedData);
               }
-            }
-          })
-          .catch((err) => {
-            message.reply("error: " + err);
-          });
-      }
-    });
+            })
+            .catch((err) => {
+              message.reply("error: " + err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   } else {
     message.reply(
       `"${command} ` +
