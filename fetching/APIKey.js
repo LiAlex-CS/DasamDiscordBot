de62890c4d8321f467d44204900eb9b3fe853775
Alex Li <lialex5883@gmail.com>
2022-06-26 15:52:32 -0400

    added val API key in fetching.js

---
+++ fetching/APIKey.js
@@ -0,0 +1,1 @@
+module.exports = { API_key: process.env.VAL_API_KEY };
--- fetching/fetching.js
+++ fetching/fetching.js
@@ -1,25 +1,24 @@
 const fetch = require("node-fetch-commonjs");
+const { key } = require("./APIKey");
+
+const rootURL = "https://api.henrikdev.xyz/valorant/v1";
+const headers = { Authorization: key };
+const APIConfigGET = { method: "GET", headers: headers };
 
 const getRankedData = async (name, id) => {
-  const resp = await fetch(
-    `https://api.henrikdev.xyz/valorant/v1/mmr/na/${name}/${id}`
-  );
+  const resp = await fetch(`${rootURL}/mmr/na/${name}/${id}`, APIConfigGET);
   const data = await resp.json();
   return data;
 };
 
 const getUserData = async (name, id) => {
-  const resp = await fetch(
-    `https://api.henrikdev.xyz/valorant/v1/account/${name}/${id}`
-  );
+  const resp = await fetch(`${rootURL}/account/${name}/${id}`, APIConfigGET);
   const data = await resp.json();
   return data;
 };
 
 const getRankedDataByPUUID = async (puuid) => {
-  const resp = await fetch(
-    `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/na/${puuid}`
-  );
+  const resp = await fetch(`${rootURL}/by-puuid/mmr/na/${puuid}`, APIConfigGET);
   const data = await resp.json();
   return data;
 };
