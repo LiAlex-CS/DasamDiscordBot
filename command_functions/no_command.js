a4ab9d30c9d7431cec7d6de8fffb4fc787491240
Alex Li <lialex5883@gmail.com>
2023-03-19 00:29:05 -0400

    added no_command function, added BOT_DESCRIPTION in command constants, modified help texts

--- command_functions/no_command.js
+++ command_functions/no_command.js
@@ -1,5 +1,7 @@
+const { BOT_DESCRIPTION } = require("../constants/commands");
+
 const no_command = (message) => {
-  message.reply("no command");
+  message.reply(BOT_DESCRIPTION);
 };
 
 module.exports = { no_command };
