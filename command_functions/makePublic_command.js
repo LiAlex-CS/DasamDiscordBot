e014823ecc5c5f4c9c28d823e2239a393892b4d4
Alex Li <lialex5883@gmail.com>
2023-04-09 01:13:28 -0400

    refactored makePublic to check for discord user and admin

--- command_functions/makePublic_command.js
+++ command_functions/makePublic_command.js
@@ -4,33 +4,43 @@ const {
   ACCOUNT_UPDATE_SUCCESS,
 } = require("../constants/commands");
 
-const { findOneByNameAndTagAndUpdate } = require("../data/mongoDb");
+const {
+  getAccountByNameAndTag,
+  isDiscordUserAdmin,
+} = require("../data/mongoDb");
 
 const { getModifiedArguments, removeHashtagFromTag } = require("../services");
 
-const makePublic_command = (message, command, argsAsString) => {
+const makePublic_command = async (message, command, argsAsString) => {
   const modifiedArgs = getModifiedArguments(argsAsString);
   if (modifiedArgs.length === 4) {
     const name = modifiedArgs[0];
     const tag = removeHashtagFromTag(modifiedArgs[1]);
     const username = modifiedArgs[2];
     const password = modifiedArgs[3];
+    const valAccount = await getAccountByNameAndTag(name, tag);
+    const isAdmin = await isDiscordUserAdmin(message.author.id);
 
-    findOneByNameAndTagAndUpdate(name, tag, (account, successSaveCallback) => {
-      if (!account) {
-        message.reply(`**${name} #${tag}** ` + COMMAND_ERRORS.not_in_db);
-      } else if (!account.private) {
-        message.reply(
-          `**${name} #${tag}** ` + COMMAND_ERRORS.makePublic_already_public
-        );
-      } else {
-        account.username = username;
-        account.password = password;
-        account.private = false;
-        successSaveCallback();
-        message.reply(ACCOUNT_UPDATE_SUCCESS);
-      }
-    });
+    if (!valAccount) {
+      message.reply(`**${name} #${tag}** ` + COMMAND_ERRORS.not_in_db);
+    } else if (message.author.id !== valAccount.creator_disc_id || !isAdmin) {
+      message.reply("not your smurf. not premeted");
+    } else if (!valAccount.private) {
+      message.reply(
+        `**${name} #${tag}** ` + COMMAND_ERRORS.makePublic_already_public
+      );
+    } else {
+      valAccount.username = username;
+      valAccount.password = password;
+      valAccount.private = false;
+      valAccount.save((err) => {
+        if (err) {
+          message.reply("error saving account status");
+        } else {
+          message.reply(ACCOUNT_UPDATE_SUCCESS);
+        }
+      });
+    }
   } else {
     message.reply(
       `"${command} ` +
