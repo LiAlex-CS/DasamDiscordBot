261e03868e3ff78c8927785946218908bde90359
Alex Li <lialex5883@gmail.com>
2023-04-09 20:53:54 -0400

    changed all setSmurf to addSmurf

--- command_functions/index.js
+++ command_functions/index.js
@@ -2,7 +2,7 @@ const { help_command } = require("./help_command");
 const { ranks_command } = require("./ranks_command");
 const { rank_command } = require("./rank_command");
 const { credentials_command } = require("./credentials_command");
-const { setSmurf_command } = require("./setSmurf_command");
+const { addSmurf_command } = require("./addSmurf_command");
 const { makePublic_command } = require("./makePublic_command");
 const { makePrivate_command } = require("./makePrivate_command");
 const { unknown_command } = require("./unknown_command");
@@ -14,7 +14,7 @@ module.exports = {
   ranks_command,
   rank_command,
   credentials_command,
-  setSmurf_command,
+  addSmurf_command,
   makePublic_command,
   makePrivate_command,
   unknown_command,
